ef53b1e8e652ea2a808da4dd08df3284d78217cd
yishaq-official <yishaq.official@gmail.com>
2026-04-25 15:26:00 +0300

    ui store implementation with zustand

---
+++ frontend/src/store/uiStore.js
@@ -0,0 +1,8 @@
+import { create } from 'zustand'
+
+export const useUiStore = create((set) => ({
+  sidebarOpen: true,
+  theme: 'light',
+  toggleSidebar: () => set((s) => ({ sidebarOpen: !s.sidebarOpen })),
+  setTheme: (theme) => set({ theme }),
+}))
