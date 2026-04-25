96c6ed0deaf567ac9388b28ac1406efbb8aef1eb
yishaq-official <yishaq.official@gmail.com>
2026-04-25 15:32:40 +0300

    making placeholder pages to keep the app from creash because of imports in route

---
+++ frontend/src/pages/auth/index.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <div className="p-8"><h1 className="text-2xl font-semibold">auth</h1></div>
+}
---
+++ frontend/src/pages/dashboard/index.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <div className="p-8"><h1 className="text-2xl font-semibold">Dashboard</h1></div>
+}
